adf2ce64c44239eacc7ec6041d5de8de4d7707d3
Alejandro Biondani <abiondani@gmail.com>
2022-10-29 01:23:39 -0300

    Filtro en template, acceso admin y ajustes CSS

--- core/views.py
+++ core/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request,"core/home.html")
+    return render(request, "core/home.html")
 
 def about_us(request):
-    return render(request,"core/about_us.html")
+    return render(request, "core/about_us.html")
 
 def contact(request):
-    return render(request,"core/contact.html")
+    return render(request, "core/contact.html")
+
