9d982db732aa683802f826d79aca8376de3c539d
Alejandro Biondani <abiondani@gmail.com>
2022-10-29 21:24:45 -0300

    Modelo terminado

--- products/views.py
+++ products/views.py
@@ -1,14 +1,10 @@
 from django.shortcuts import render
-# from .models import Product
 from .models import Category
 
-
-# Create your views here.
-
-# def products(request):
-#     products = Product.objects.all()
-#     return render(request, "products/products.html", {'products': products})
-
 def products(request):
-    categories = Category.objects.all()
-    return render(request, "products/products.html", {'categories': categories})
+    categories = Category.objects.all().order_by('title')
+    return render(
+        request, 
+        "products/products.html", 
+        {'categories': categories}
+    )
